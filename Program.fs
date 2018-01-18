eb5debf4f465b3015652dc1427289466b136695d
JamesMReilly <jimreilly28@gmail.com>
2018-01-18 12:26:38 -0600

    corrected autoformat tab size in Program.fs to match preference

--- Program.fs
+++ Program.fs
@@ -6,11 +6,12 @@ open NameGenerator.NameSelector
 
 [<EntryPoint>]
 let main argv =
-    let foo = FirstNames
-    let snap = foo.[0]
-    let bar = foo.[0].[0]
-    printfn "%s\n" bar
+  let foo = FirstNames
+  let snap = foo.[0]
+  let bar = foo.[0].[0]
+  
+  printfn "%s\n" bar
 
-    let crackle = SelectFirstName 0.
-    printfn "%s\n" crackle
-    0 // return an integer exit code
+  let crackle = SelectFirstName 0.
+  printfn "%s\n" crackle
+  0 // return an integer exit code
