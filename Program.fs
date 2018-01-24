82a2a1731d9d8490c49c258685c86575c2dbc7cf
JamesMReilly <jimreilly28@gmail.com>
2018-01-23 22:04:52 -0600

    now generates N random names, where N is a real positive integer CLI argument

--- Program.fs
+++ Program.fs
@@ -7,14 +7,27 @@ open NameGenerator.NameSelector
 let tupleRandomFloats (randomObject: System.Random) =
   (randomObject.NextDouble() * 100. , randomObject.NextDouble() * 100.)
 
+let generateARandomName (firstSeed, lastSeed) =
+  let firstName = SelectFirstName firstSeed
+  let lastName = SelectLastName lastSeed
+  printfn "%s %s" firstName lastName
+
+let generateNRandomNames rnd n =
+  for i in 1 .. n do
+    rnd |> tupleRandomFloats |> generateARandomName
+
+let (|Int|_|) input = 
+  match Int32.TryParse input with
+  | (true, i) when i > 0 -> Some i
+  | (false, _) -> None
 
 [<EntryPoint>]
 let main argv =
   let rnd = new Random()
-  let (firstSeed,lastSeed) = rnd |> tupleRandomFloats
-  let firstName = SelectFirstName firstSeed
-  let lastName = SelectLastName lastSeed
 
-  printfn "%s %s\n" firstName lastName
-
+  match argv with
+    | [| Int n |] when n > 0 -> n |> generateNRandomNames rnd
+    | _ -> 1 |> generateNRandomNames rnd
+  
+  
   0 // return an integer exit code
